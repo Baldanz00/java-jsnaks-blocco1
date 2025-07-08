2970ba25d4e8b27111eb56deacf83de59e274d68
Baldanz00 <gioia.travaglini18@gmail.com>
2025-07-08 18:17:21 +0200

    Snack9.java
    somma e media dei primi 10 numeri

---
+++ Snack9.java
@@ -0,0 +1,10 @@
+public class Snack9 {
+  public static void main (String[] args) {
+    int somma = 0;
+    for (int i=1; i<=10; i++) {
+      somma +=1;
+    }
+    double media =somma / 10.0;
+    System.out.println("Somma: " + somma);
+     System.out.println("Media: " + media);
+  }
