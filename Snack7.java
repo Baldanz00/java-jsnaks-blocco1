18065ee6c6080d0ca2ac553218c038ae32ef65d2
Baldanz00 <gioia.travaglini18@gmail.com>
2025-07-08 18:08:00 +0200

    Snack7.java
    potenze di 2 fino a 1000

---
+++ Snack7.java
@@ -0,0 +1,9 @@
+public class Snack7 {
+  oublic static void main (String[] args){
+    int potenza =!;
+    while (potenza<=1000) {
+      System.out.println(potenza);
+      potenza *=2;
+    }
+  }
+}
